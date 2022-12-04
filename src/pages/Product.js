dbd4525a65570951c499c0bb4a6642cefa2e3a6d
Drslx <drslxprogramador@gmail.com>
2022-12-03 23:06:56 -0300

    dynamic loading data

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,12 +1,25 @@
 import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 
 const Product = () => {
+  // Rota dinamica
   const { id } = useParams();
-  return (
-    // Rota dinamica
 
+  // Carregamento dado individual
+  const url = "http://localhost:3000/products/" + id;
+  const { data: product, loading, error } = useFetch(url);
+  return (
     <>
       <p> ID do produto: {id} </p>
+
+      {error && <p>Ocorreu um erro...</p>}
+      {loading && <p>Carregando</p>}
+      {product && (
+        <div>
+          <h1>{product.name}</h1>
+          <p>R${product.price}</p>
+        </div>
+      )}
     </>
   );
 };
